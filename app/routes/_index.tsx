e455cf71c528e2ac44187bea4952316fdfd309e9
Lev Chelyadinov <leva181777@gmail.com>
2023-05-27 16:42:37 +0200

    Install and run Prettier

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,14 +1,15 @@
 import type { V2_MetaFunction } from "@remix-run/node";
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "FSD Notes" },
-    { name: "description", content: "Make notes and synchronize them with the cloud!" },
-  ];
+	return [
+		{ title: "FSD Notes" },
+		{
+			name: "description",
+			content: "Make notes and synchronize them with the cloud!",
+		},
+	];
 };
 
 export default function Index() {
-  return (
-    <div className="m-4">Start here!</div>
-  );
+	return <div className="m-4">Start here!</div>;
 }
